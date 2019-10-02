71b4a11009319081bedc709083652e9e014dae0c
Malikovich <md.malikovich@gmail.com>
2019-10-02 21:58:58 +0600

    3 from 3 = Done

---
+++ app/src/main/java/com/android2_lesson1/ViewHolder.java
@@ -0,0 +1,23 @@
+package com.android2_lesson1;
+
+import android.view.View;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+
+    TextView vhTitle;
+    TextView vhDesc;
+
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
+        vhTitle = itemView.findViewById(R.id.vh_title);
+        vhDesc = itemView.findViewById(R.id.vh_desc);
+    }
+
+    public void onBind(Task task) {
+        vhTitle.setText(task.getTitle());
+        vhDesc.setText(task.getDesc());
+    }
+}
